a6d9fd754d961188992a0b06806870f8326a89df
Alessio Franceschelli <alessio@franceschelli.me>
2018-12-23 20:24:12 +0000

    Added Xml docs

--- src/HttpClientLab.Extensions.Moq/WebApplicationFactoryExtensions.cs
+++ src/HttpClientLab.Extensions.Moq/WebApplicationFactoryExtensions.cs
@@ -5,6 +5,13 @@ namespace HttpClientLab
 {
     public static class WebApplicationFactoryExtensions
     {
+        /// <summary>
+        /// Configure the HttpClientFactory of the in-memory testing environemnt to use the mocked behaviour returned as out parameter.
+        /// </summary>
+        /// <typeparam name="TEntryPoint">The type of the application entry point, usually Startup.</typeparam>
+        /// <param name="factory">The WebApplicationFactory instance.</param>
+        /// <param name="httpClientBehaviour">The behaviour of the HttpClients to be configured.</param>
+        /// <returns></returns>
         public static WebApplicationFactory<TEntryPoint> WithHttpClientBehaviour<TEntryPoint>(
             this WebApplicationFactory<TEntryPoint> factory,
             out Mock<IHttpClientBehaviour> httpClientBehaviour)
