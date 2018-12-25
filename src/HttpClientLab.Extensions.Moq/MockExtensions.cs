9b9e8f0e23da2304d876411bc38c0ef007e12b1f
Alessio Franceschelli <alessio@franceschelli.me>
2018-12-25 19:00:40 +0000

    XML Documentation and improved README (#2)

--- src/HttpClientLab.Extensions.Moq/MockExtensions.cs
+++ src/HttpClientLab.Extensions.Moq/MockExtensions.cs
@@ -7,19 +7,41 @@ namespace HttpClientLab
     public static class MockExtensions
     {
         static readonly bool HasNotWindowsNewLine = Environment.NewLine != "\r\n";
-        
+
+        /// <summary>
+        /// Create a builder for the behaviour of any client.
+        /// </summary>
+        /// <param name="httpServer">The behaviour to setup, obtained with WithHttpClientBehaviour on the WebApplicationFactory.</param>
+        /// <returns>The builder to setup the behaviour for any client.</returns>
         public static ISetupClient SetupForAnyClient(this Mock<IHttpClientBehaviour> httpServer) =>
             new SetupClient(httpServer);
 
-        public static ISetupClient SetupForClient(this Mock<IHttpClientBehaviour> httpServer, string httpClientName) =>
-            new SetupClient(httpServer, httpClientName);
+        /// <summary>
+        /// Create a builder for the behaviour of the specified named client.
+        /// </summary>
+        /// <param name="httpClientBehaviour">The behaviour to setup, obtained with WithHttpClientBehaviour on the WebApplicationFactory.</param>
+        /// <param name="httpClientName">The name of the client to setup.</param>
+        /// <returns>The builder to setup the behaviour of the named client.</returns>
+        public static ISetupClient SetupForClient(this Mock<IHttpClientBehaviour> httpClientBehaviour, string httpClientName) =>
+            new SetupClient(httpClientBehaviour, httpClientName);
 
-        public static ISetupClient SetupForClient<T>(this Mock<IHttpClientBehaviour> httpServer) =>
-            SetupForClient(httpServer, typeof(T).Name);
+        /// <summary>
+        /// Create a builder for the behaviour of the specified typed client.
+        /// </summary>
+        /// <typeparam name="TClient">The type of the client to setup.</typeparam>
+        /// <param name="httpClientBehaviour">The behaviour to setup, obtained with WithHttpClientBehaviour on the WebApplicationFactory.</param>
+        /// <returns>The builder to setup the behaviour of the named client.</returns>
+        public static ISetupClient SetupForClient<TClient>(this Mock<IHttpClientBehaviour> httpClientBehaviour) =>
+            SetupForClient(httpClientBehaviour, typeof(TClient).Name);
 
-        public static void WriteHttpRequests(this Mock<IHttpClientBehaviour> mock, ITestOutputHelper output)
+        /// <summary>
+        /// Write to the provided output the requests and responses received by the mocked behaviour.
+        /// </summary>
+        /// <param name="httpClientBehaviour">The behaviour to setup, obtained with WithHttpClientBehaviour on the WebApplicationFactory.</param>
+        /// <param name="output">The output helper of the XUnit tests</param>
+        public static void WriteHttpRequests(this Mock<IHttpClientBehaviour> httpClientBehaviour, ITestOutputHelper output)
         {
-            foreach (var invocation in mock.Invocations)
+            foreach (var invocation in httpClientBehaviour.Invocations)
             {
                 foreach (var arg in invocation.Arguments)
                 {
